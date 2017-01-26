53e7eab60011569cd7c2399da5cee793da63ba09
Пономарев Никита <nikita.ponomarev@norbit.ru>
2017-01-26 21:25:46 +0300

    Created solution

---
+++ Store.Domain/Contracts/IProductService.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+using System.Dynamic;
+using Store.Domain.Models;
+
+namespace Store.Domain.Contracts
+{
+    interface IProductService
+    {
+        Product Get(int id);
+
+        List<Product> GetAll();
+
+        void Add(Product product);
+
+        void Update(Product product);
+
+        void Delete(int id);
+
+    }
+}
---
+++ Store.Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
