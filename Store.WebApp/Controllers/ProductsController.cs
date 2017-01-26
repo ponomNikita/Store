7758fcb393b3383bb37dd7ac20854d22ed06ab9f
Пономарев Никита <nikita.ponomarev@norbit.ru>
2017-01-26 21:59:52 +0300

    Configurated log4net

--- Store.WebApp/Controllers/ProductsController.cs
+++ Store.WebApp/Controllers/ProductsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Store.Infrastructure.Logging;
 
 namespace Store.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ILogger _logger;
+
+        public ProductsController()
+        {
+            _logger = LoggerManager.GetLogger(GetType());
+        }
         // GET: Products
         public ActionResult Index()
         {
+            _logger.Info("Index()");
             return View();
         }
     }
